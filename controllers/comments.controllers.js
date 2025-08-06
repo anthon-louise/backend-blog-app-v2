01adfb6454c8ec18ee077b6909e15eae59fd378f
anthon-louise <anthonlouisenoynay123@gmail.com>
2025-08-06 18:26:03 +0800

    feature: fetch comments

--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -4,12 +4,10 @@ const validationSchema = require('../validation/schema')
 
 const createComment = asyncHandler(async (req, res) => {
     const { userId } = req.user
-    const bodyValue = await  validationSchema.commentSchema.validateAsync(req.body)
+    const bodyValue = await validationSchema.commentSchema.validateAsync(req.body)
     const { content } = bodyValue
     const idValue = await validationSchema.idSchema.validateAsync(req.params)
     const postId = idValue.id
-
-    console.log(postId)
 
     const [rows] = await pool.query(`
         SELECT * FROM posts WHERE id=?
@@ -25,10 +23,46 @@ const createComment = asyncHandler(async (req, res) => {
         VALUES (?, ?, ?)
         `, [content, postId, userId])
 
-    res.json({message: 'Commented'})
+    res.json({ message: 'Commented' })
 })
 
+const getComments = asyncHandler(async (req, res) => {
+    const value = await validationSchema.idSchema.validateAsync(req.params)
+    const postId = value.id
+
+    const [postRows] = await pool.query(`
+        SELECT * FROM posts WHERE id=?
+        `, [postId])
+    if (postRows.length === 0) {
+        const err = new Error('Post not found')
+        err.status = 404
+        throw err
+    }
+
+    const [commentRows] = await pool.query(`
+        SELECT * FROM comments WHERE post_id=?
+        `, postId)
+    res.json(commentRows)
+})
+
+const getCommentById = asyncHandler(async (req, res) => {
+    const value = await validationSchema.idSchema.validateAsync(req.params)
+    const commentId = value.id
+
+    const [rows] = await pool.query(`
+        SELECT * FROM comments WHERE id=?
+        `, commentId)
+    if (rows.length === 0) {
+        const err = new Error('No comment found')
+        err.status = 404
+        throw err
+    }
+
+    res.json(rows)
+})
 
 module.exports = {
-    createComment
+    createComment,
+    getComments,
+    getCommentById
 }
--- routes/comments.routes.js
+++ routes/comments.routes.js
@@ -4,5 +4,7 @@ const commentsControllers = require('../controllers/comments.controllers')
 const auth = require('../middlewares/auth')
 
 router.post('/:id', auth, commentsControllers.createComment)
+router.get('/:id', auth, commentsControllers.getComments)
+router.get('/single/:id', auth, commentsControllers.getCommentById)
 
 module.exports = router
